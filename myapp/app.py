16d18ea7af9f87af3e2c4057979803bb02bdb49a
Pranav Bobde <pranav@Pranavs-MacBook-Air.local>
2023-12-29 14:45:09 +0530

    basic flask app

--- myapp/app.py
+++ myapp/app.py
@@ -1,2 +1,13 @@
+# setup a flask app with / endpoint
 
-print('Hello World!')
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Hello, World!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
