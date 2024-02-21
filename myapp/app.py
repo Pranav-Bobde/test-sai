943235229168076f7d33a3a82a355ac67f6448f1
Pranav-Bobde <bobdep31@gmail.com>
2024-02-21 10:47:14 +0530

    feat: :sparkles: print the IP of request

--- myapp/app.py
+++ myapp/app.py
@@ -1,2 +1,18 @@
+from flask import Flask, request, render_template_string
 
-print('Hello World!')
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    # Extract the requester's IP address
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        user_ip = request.environ['REMOTE_ADDR']
+    else:
+        user_ip = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
+    # Log the IP address to the console
+    print(f"Request received from IP: {user_ip}")
+    # Return the IP address in the HTML response
+    return render_template_string('<h1>Hello! Your IP address is {{user_ip}}</h1>', user_ip=user_ip)
+
+if __name__ == '__main__':
+    app.run(debug=True)
